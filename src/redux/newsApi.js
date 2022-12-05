1bee15027b36f7a1923b7ed6e9d278653fe3afd5
DmitriyGovosyants <Demiangvs@gmail.com>
2022-12-05 16:32:17 +0200

    change server

--- src/redux/newsApi.js
+++ src/redux/newsApi.js
@@ -3,21 +3,21 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const newsApi = createApi({
     reducePath: 'newsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://petly-node-rest-api.onrender.com/api/',
+        baseUrl: 'https://petly-node-js-rest-api-v2.onrender.com/api',
     }),
     tagTypes: ['News'],
     endpoints: builder => ({
         getAllNews: builder.query({
             query: (newsName) => {
                 if (newsName !== '') {
-                    return `news?search=${newsName}`;
+                    return `/news?search=${newsName}`;
                 }
                 return 'news';
             },
             providesTags: ['News'],
         }),
         getSearchNews: builder.query({
-            query: newsName => `news?search=${newsName}`,
+            query: newsName => `/news?search=${newsName}`,
             providesTags: ['News'],
         }),
     })
