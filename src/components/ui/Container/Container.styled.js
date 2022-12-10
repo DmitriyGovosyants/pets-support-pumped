4fc471220fda131775bc07be540d9d1b0a82ad33
Dmitriy Govosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-12-10 14:23:32 +0200

    Update Container.styled.js

--- src/components/ui/Container/Container.styled.js
+++ src/components/ui/Container/Container.styled.js
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 import { device, size } from 'styles/mediaquery';
 
 export const ContainerStyled = styled.div`
-  position: relative;
   width: 100%;
   margin: 0 auto;
   padding-left: ${p => p.theme.spacing(5)};
