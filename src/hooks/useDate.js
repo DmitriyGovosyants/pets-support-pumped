fe3aae96550eaf4f610d3726170f6e40277685ca
DmitriyGovosyants <Demiangvs@gmail.com>
2022-12-11 23:07:39 +0200

    fix: useDate

--- src/hooks/useDate.js
+++ src/hooks/useDate.js
@@ -5,21 +5,24 @@ export const useDate = (birthdate) => {
 
   useEffect(() => {
     if (!birthdate || birthdate === '') {
-      setState('-');
-    } else {
-      const array = birthdate.split('.');
-      const newBirthdayFormat = [array[1], array[0], array[2]].join('-');
-      const petYears = Math.floor(
-        (new Date() - new Date(newBirthdayFormat)) /
-          (1000 * 60 * 60 * 24 * 365.25)
-      );
-      if (petYears === 0) {
-        setState('Less the one year');
-      } else if (petYears === 1) {
-        setState('One year');
-      } else if (petYears > 1) {
-        setState(`${petYears} years`);
-      }
+      return setState('-');
+    } 
+
+    const array = birthdate.split('.');
+    const newBirthdayFormat = [array[1], array[0], array[2]].join('-');
+    const petYears = Math.floor(
+      (new Date() - new Date(newBirthdayFormat)) /
+        (1000 * 60 * 60 * 24 * 365.25)
+    );
+
+    if (petYears === 0) {
+      setState('Less the one year');
+    }
+    if (petYears === 1) {
+      setState('One year');
+    }
+    if (petYears > 1) {
+      setState(`${petYears} years`);
     }
   }, [birthdate]);
 
