ce22474f5240e44e62b87e3a2cc6a9b61789588b
Dmitriy Govosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-12-11 15:07:46 +0200

    Update noticesApi.js

--- src/redux/noticesApi.js
+++ src/redux/noticesApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const noticesApi = createApi({
-  reducerPath: 'noticesApi',
+  reducerPath: 'notices',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://petly-node-js-rest-api-v2.onrender.com/api',
     prepareHeaders: (headers, { getState }) => {
