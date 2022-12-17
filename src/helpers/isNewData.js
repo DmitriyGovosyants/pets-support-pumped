d35b4b7ca842ccaf2ca0435e7946d9511723d959
DmitriyGovosyants <Demiangvs@gmail.com>
2022-12-17 21:34:39 +0200

    fix: phone validation

--- src/helpers/isNewData.js
+++ src/helpers/isNewData.js
@@ -6,9 +6,21 @@ export const isNewData = (object1, object2) => {
   for (let i = 0; i < props1.length; i += 1) {
     const prop = props1[i];
 
-    if (object1[prop] !== object2[prop]) {
-      return newData = true;
+    if (prop === 'phone') {
+      const currentPhone = object1[prop].split(/[-()]+/).join('');
+      const dataPhone = object2[prop].split(/[-()]+/).join('');
+
+      if (currentPhone !== dataPhone) {
+        return true;
+      }
+    }
+
+    if (prop !== 'phone') {
+      if (object1[prop] !== object2[prop]) {
+        return true;
+      }
     }
   }
+
   return newData;
 }
